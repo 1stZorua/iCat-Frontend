a5a8bfd626b3815335d66bf6781b52f5853003dd
Zorua <milandehaart@live.nl>
2024-11-12 00:19:40 +0100

    Added flash type to PageData

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { FlashType } from '$lib/types/types';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			flash?: { type: FlashType; message: string };
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
