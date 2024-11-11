72a61b75d5514136ba3092be1ed1a3049d8347e4
Zorua <milandehaart@live.nl>
2024-11-11 20:07:08 +0100

    Added base button component

---
+++ src/lib/components/shared/buttons/Base.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import type { Snippet } from 'svelte';
+import { cn } from '$lib/utils/merge';
+import type { ClassType } from '$components/_types';
+import { type ButtonProps, buttonVariants } from './variants';
+import type { HTMLButtonAttributes } from 'svelte/elements';
+
+interface Props extends HTMLButtonAttributes {
+	className?: ClassType;
+	href?: string;
+	props?: ButtonProps;
+	children?: Snippet;
+}
+
+let { className, href, props, children, ...rest }: Props = $props();
+</script>
+
+{#if href}
+	<a href={href} class={cn(buttonVariants({ ...props }), className)}>
+		{@render children?.()}
+	</a>
+{:else}
+	<button class={cn(buttonVariants({ ...props }), className)} {...rest}>
+		{@render children?.()}
+	</button>
+{/if}
