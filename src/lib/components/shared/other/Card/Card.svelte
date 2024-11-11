45e96efe8337818b3512af8ada8f18ab517f79f0
Zorua <milandehaart@live.nl>
2024-11-12 00:20:50 +0100

    Extend card component props with HTMLAnchorAttributes

--- src/lib/components/shared/other/Card/Card.svelte
+++ src/lib/components/shared/other/Card/Card.svelte
@@ -3,17 +3,20 @@ import type { Snippet } from 'svelte';
 import { cn } from '$lib/utils/merge';
 import type { ClassType } from '$components/_types';
 import { type CardProps, cardVariants } from './variants';
+import type { HTMLAnchorAttributes } from 'svelte/elements';
 
-let {
-	className,
-	href = '',
-	props,
-	children
-}: { className?: ClassType; href?: string; props?: CardProps; children?: Snippet } = $props();
+interface Props extends HTMLAnchorAttributes {
+	className?: ClassType;
+	href?: string;
+	props?: CardProps;
+	children?: Snippet;
+}
+
+let { className, href = '', props, children, ...rest }: Props = $props();
 </script>
 
 {#if href}
-	<a href={href} class={cn(cardVariants(props), className)}>
+	<a href={href} class={cn(cardVariants(props), className)} {...rest}>
 		{@render children?.()}
 	</a>
 {:else}
