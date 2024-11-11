e7037b4f8035953326520a901ff0807126d56a2e
Zorua <milandehaart@live.nl>
2024-11-11 20:10:02 +0100

    Added Message and VectorDBResult types

---
+++ src/lib/types/types.ts
@@ -0,0 +1,17 @@
+export interface Message {
+	sender: 'user' | 'bot';
+	text: string;
+	typedText?: string;
+}
+
+export interface VectorDBResult {
+	id: string;
+	metadata: {
+		characters: string;
+		id: string;
+		pages: string;
+		pdf_name: string;
+		text: string;
+	};
+	score: number;
+}
