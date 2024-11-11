30315db3f133c00f6ea117b03f5c344529c91a7d
Zorua <milandehaart@live.nl>
2024-11-11 20:08:00 +0100

    Added primary button component

---
+++ src/lib/components/shared/buttons/variants/Primary.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type { ClassType } from '$components/_types';
+import type { Snippet } from 'svelte';
+import type { HTMLButtonAttributes } from 'svelte/elements';
+import { Button } from '../index';
+
+interface Props extends HTMLButtonAttributes {
+	className?: ClassType;
+	href?: string;
+	children?: Snippet;
+}
+
+let { className, href, children, ...rest }: Props = $props();
+</script>
+
+<Button {...rest} className={className} href={href} props={{ variant: 'primary' }}>
+	{@render children?.()}
+</Button>
