69d6c0de286920713b8f2e2c76d96041b0ef85ad
Zorua <milandehaart@live.nl>
2024-11-11 20:09:11 +0100

    Added text heading component

---
+++ src/lib/components/shared/text/variants/Heading.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { ClassType } from '$components/_types';
+	import type { Snippet } from 'svelte';
+	import { Text } from '../index';
+
+	let { className, children }: { className?: ClassType; children?: Snippet } = $props();
+</script>
+
+<Text {className} props={{ size: 'xl', weight: 'semibold' }}>
+	{@render children?.()}
+</Text>
