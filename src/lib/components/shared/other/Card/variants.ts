a29c13403610ad6ed5996d1de0943e271b4453a4
Zorua <milandehaart@live.nl>
2024-11-11 20:07:40 +0100

    Added card variants configuration

---
+++ src/lib/components/shared/other/Card/variants.ts
@@ -0,0 +1,21 @@
+import { tv, type VariantProps } from 'tailwind-variants';
+
+const cardVariants = tv({
+	base: 'p-4 rounded-lg flex drop-shadow-sm',
+	variants: {
+		color: {
+			primary: 'bg-light-cards-primary-bg text-light-cards-primary-fg',
+			secondary: 'bg-light-cards-secondary-bg text-light-cards-secondary-fg',
+			accent: 'bg-light-cards-accent-bg text-light-cards-accent-fg',
+			background: 'bg-light-cards-background-bg text-light-cards-background-fg',
+			neutral: 'bg-light-cards-neutral-bg text-light-cards-neutral-fg'
+		}
+	},
+	defaultVariants: {
+		color: 'neutral'
+	}
+});
+
+type CardProps = VariantProps<typeof cardVariants>;
+
+export { cardVariants, type CardProps };
