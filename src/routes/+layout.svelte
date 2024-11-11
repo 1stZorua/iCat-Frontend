0d0c6a353759c70a26daa184de7f46cb4226ce3a
Zorua <milandehaart@live.nl>
2024-11-11 20:11:34 +0100

    Added iconify-icon import statement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import '../app.css';
+import type { Snippet } from 'svelte';
+import 'iconify-icon';
+import '../app.css';
 
-	let { 
-		children 
-	}: { children?: Snippet } = $props();
+let { children }: { children?: Snippet } = $props();
 </script>
 
 {@render children?.()}
