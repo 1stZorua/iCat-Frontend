795dffde2a237c8460bc61f4fc94ebc3d1defffd
Zorua <milandehaart@live.nl>
2024-11-12 00:24:19 +0100

    Integrate PageLayout component, remove unnecessary imports, and remove work-in-progress thinking animation for the bot

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
-import { enhance } from '$app/forms';
-import { ButtonAction, ButtonPrimary } from '$components/shared/buttons';
-import { Icon } from '$components/shared/other';
-import Avatar from '$components/shared/other/Avatar.svelte';
-import Input from '$components/shared/other/Input.svelte';
-import { TextBase, TextLarge } from '$components/shared/text';
-import type { Message } from '$lib/types/types';
 import { untrack } from 'svelte';
+import { enhance } from '$app/forms';
 import { fade } from 'svelte/transition';
+import { PageLayout } from '$components/page';
+import { ButtonPrimary } from '$components/shared/buttons';
+import { Avatar, Icon, Input } from '$components/shared/other';
+import { TextBase } from '$components/shared/text';
+import type { Message } from '$lib/types/types';
 
 let { form }: { form: { message: Message } } = $props();
-
 let elForm: HTMLFormElement;
 let messages: Message[] = $state([
 	{
@@ -18,16 +16,13 @@ let messages: Message[] = $state([
 		text: 'Hi, Emma. I am iCat and I can help you with any questions regarding your museum visit. Is there something I can do for you?'
 	}
 ]);
-
 let lastMessage: Message | null = null;
-let processingIndex: number = $state(-1);
-let isTalking: boolean = $state(false);
+let isProcessing: boolean = $state(false);
 
 $effect(() => {
 	if (form?.message && form.message !== lastMessage) {
 		messages = [...messages, form.message];
 		lastMessage = form.message;
-		processingIndex = -1;
 	}
 
 	if (messages.length > 1) {
@@ -45,69 +40,54 @@ async function typeMessage(index: number) {
 		return;
 	}
 
-	isTalking = true;
-
 	for (let i = 0; i <= message.text.length; i++) {
 		message.typedText = message.text.slice(0, i);
 		await new Promise((resolve) => setTimeout(resolve, 20));
 	}
 
-	isTalking = false;
+	isProcessing = false;
 }
 
 async function onSubmit() {
 	const elPrompt = elForm.querySelector('input');
 
-	if (!elPrompt || isTalking) return;
+	if (!elPrompt || isProcessing) return;
 
 	const userMessage: Message = { sender: 'user', text: elPrompt.value as string };
 	elPrompt.value = '';
 
 	messages = [...messages, userMessage];
-
-	processingIndex = messages.length - 1;
+	isProcessing = true;
 }
 </script>
 
-<section class="wrapper relative flex h-full w-full flex-col overflow-scroll">
-	<div class="wrapper fixed left-0 top-0 flex w-full items-center">
-		<ButtonAction className="absolute top-1/2 -translate-y-1/2">
-			<Icon icon="lucide:chevron-left"></Icon>
-		</ButtonAction>
-		<TextLarge className="mx-auto h-12 flex items-center">Chat</TextLarge>
+<PageLayout page="Chat">
+	<div class={`flex gap-2`}>
+		<Avatar
+			className="flex-shrink-0 justify-center items-end bg-light-cards-neutral-bg"
+			imageClassName="h-5/6"
+			src="/images/icat.png"
+		></Avatar>
+		<TextBase className="font-normal p-3 rounded-md bg-light-background-secondary"
+			>{messages[0].text}</TextBase
+		>
 	</div>
-	<div class="mb-[5.5rem] mt-[4.5rem] flex flex-col gap-5">
-		<div transition:fade class={`flex gap-2`}>
-			<Avatar
-				className="flex-shrink-0 justify-center items-end bg-light-cards-neutral-bg"
-				imageClassName="h-5/6"
-				src="/images/icat.png"
-			></Avatar>
-			<TextBase className="font-normal p-3 rounded-md bg-light-background-secondary"
-				>{messages[0].text}</TextBase
+	{#each messages.slice(1) as msg}
+		{@const isBot = msg.sender == 'bot'}
+		<div in:fade class={`flex gap-2 ${isBot ? '' : 'justify-end'}`}>
+			{#if isBot}
+				<Avatar
+					className="flex-shrink-0 justify-center items-end bg-light-cards-neutral-bg"
+					imageClassName="h-5/6"
+					src="/images/icat.png"
+				></Avatar>
+			{/if}
+			<TextBase
+				className={`font-normal p-3 rounded-md ${isBot ? 'bg-light-background-secondary' : 'bg-light-cards-neutral-bg text-white'}`}
+				>{msg.typedText}</TextBase
 			>
 		</div>
-		{#each messages.slice(1) as msg, index}
-			{@const isBot = msg.sender == 'bot'}
-			<div transition:fade class={`flex gap-2 ${isBot ? '' : 'justify-end'}`}>
-				{#if isBot}
-					<Avatar
-						className="flex-shrink-0 justify-center items-end bg-light-cards-neutral-bg"
-						imageClassName="h-5/6"
-						src="/images/icat.png"
-					></Avatar>
-				{/if}
-				{#if processingIndex == index}
-					<TextBase>Loading ...</TextBase>
-				{:else}
-					<TextBase
-						className={`font-normal p-3 rounded-md ${isBot ? 'bg-light-background-secondary' : 'bg-light-cards-neutral-bg text-white'}`}
-						>{msg.typedText}</TextBase
-					>
-				{/if}
-			</div>
-		{/each}
-	</div>
+	{/each}
 	<form
 		onsubmit={onSubmit}
 		bind:this={elForm}
@@ -117,12 +97,12 @@ async function onSubmit() {
 		use:enhance
 	>
 		<Input type="text" name="prompt" placeholder="Type something..." required>
-			<ButtonPrimary className="p-3">
+			<ButtonPrimary className="p-3" disabled={isProcessing}>
 				<Icon
 					className="text-white"
-					icon={(processingIndex !== -1 || isTalking) ? 'svg-spinners:90-ring-with-bg' : 'lucide:send'}
+					icon={isProcessing ? 'svg-spinners:90-ring-with-bg' : 'lucide:send'}
 				></Icon>
 			</ButtonPrimary>
 		</Input>
 	</form>
-</section>
+</PageLayout>
