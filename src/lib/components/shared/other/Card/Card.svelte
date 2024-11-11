78523a67160aa067f360d75a76daa886a13d04fa
Zorua <milandehaart@live.nl>
2024-11-11 20:08:46 +0100

    Added card component

---
+++ src/lib/components/shared/other/Card/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import type { Snippet } from 'svelte';
+import { cn } from '$lib/utils/merge';
+import type { ClassType } from '$components/_types';
+import { type CardProps, cardVariants } from './variants';
+
+let {
+	className,
+	href = '',
+	props,
+	children
+}: { className?: ClassType; href?: string; props?: CardProps; children?: Snippet } = $props();
+</script>
+
+{#if href}
+	<a href={href} class={cn(cardVariants(props), className)}>
+		{@render children?.()}
+	</a>
+{:else}
+	<div class={cn(cardVariants(props), className)}>
+		{@render children?.()}
+	</div>
+{/if}
