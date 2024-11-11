ba5f3b9a1886e1299f81120d41aca514f364d472
Zorua <milandehaart@live.nl>
2024-11-11 20:08:19 +0100

    Added avatar component

---
+++ src/lib/components/shared/other/Avatar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type { ClassType } from '$components/_types';
+import { cn } from '$lib/utils/merge';
+
+let {
+	className,
+	imageClassName,
+	src,
+	alt = 'Avatar'
+}: { className?: ClassType; imageClassName?: string; src?: string; alt?: string } = $props();
+</script>
+
+<div class={cn('flex h-12 w-12 overflow-hidden rounded-full', className)}>
+	{#if src}
+		<img class={cn('', imageClassName)} src={src} alt={alt} />
+	{/if}
+</div>
