05ede5145a987882ff249e8d54dde19ace3d5d29
Zorua <milandehaart@live.nl>
2024-11-11 20:08:24 +0100

    Added icon component

---
+++ src/lib/components/shared/other/Icon.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { cn } from '$lib/utils/merge';
+import type { ClassType } from '$components/_types';
+
+let { className, icon }: { className?: ClassType; icon: string } = $props();
+</script>
+
+<iconify-icon class={cn("text-xl", className)} icon={icon}></iconify-icon>
