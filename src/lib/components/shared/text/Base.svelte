7748f2d70d5d632a6cedd3cdc22ff91cc790d699
Zorua <milandehaart@live.nl>
2024-11-11 20:08:58 +0100

    Added base text component

---
+++ src/lib/components/shared/text/Base.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { Snippet } from 'svelte';
+import { cn } from '$lib/utils/merge';
+import type { ClassType } from '$components/_types';
+import { type TextProps, textVariants } from './variants';
+
+let {
+	className,
+	props,
+	children
+}: { className?: ClassType; props?: TextProps; children?: Snippet } = $props();
+</script>
+
+<span class={cn(textVariants(props), className)}>
+	{@render children?.()}
+</span>
