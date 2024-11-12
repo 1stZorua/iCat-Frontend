950a7941995c74cb68a4ebf8a577cace650eabc2
2ndZorua <485892@student.fontys.nl>
2024-11-12 12:53:26 +0100

    Changed styling to match the design closely

--- src/lib/components/page/Layout.svelte
+++ src/lib/components/page/Layout.svelte
@@ -14,14 +14,15 @@ let {
 }: { className?: ClassType; href?: string; page?: string; children?: Snippet } = $props();
 </script>
 
-<section class={cn('wrapper relative flex h-full w-full flex-col overflow-scroll', className)}>
+<section class="wrapper relative flex h-full w-full flex-col overflow-scroll">
 	<div class="wrapper fixed left-0 top-0 flex w-full items-center">
 		<ButtonAction href={href} className="absolute top-1/2 -translate-y-1/2">
 			<Icon icon="lucide:chevron-left"></Icon>
 		</ButtonAction>
 		<TextLarge className="mx-auto h-12 flex items-center">{page}</TextLarge>
 	</div>
-	<div class="mb-[5.5rem] mt-[4.5rem] flex flex-col gap-5">
+	<div class={cn('mt-[4.5rem] h-full flex flex-col gap-5', className)}>
 		{@render children?.()}
 	</div>
 </section>
+	
