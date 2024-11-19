2f9d83ea6fdc7931c3a2243eaf05601b8f18afc8
2ndZorua <485892@student.fontys.nl>
2024-11-19 13:52:51 +0100

    Changed the way to handle history

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -8,27 +8,29 @@ export const actions = {
 		const data = await request.formData();
 		const prompt = data.get('prompt') as string;
 		const history = JSON.parse(data.get('history') as string) as Message[];
+		const context = JSON.parse(data.get('context') as string) as Array<string[]> ?? '';
 
 		try {
+			// prompt = `Context:\n${context.join('\n\n')}\n\nQuestion: ${prompt}`;
+			// const fullPrompt = `Context:\n${context.join('\n\n')}\n\nQuestion: ${prompt}`;
+			
 			const queryEmbedding = await getEmbeddingsFromJina(prompt);
 			const vectorDBResults = await fetchPineconeResults(fetch, queryEmbedding, 16);
 			const contextTexts = vectorDBResults.matches.map(
 				(match: VectorDBResult) => match.metadata.text
 			);
 
-			const fullContext = [
-				...history.map((msg) => msg.text),
-				...contextTexts,
-			];
+			const fullContext = context.concat(contextTexts);
 
-			const responseMessage = await sendToOpenAI(prompt, fullContext);
+			const responseMessage = await sendToOpenAI(prompt, contextTexts, history.slice(2, history.length));
 
 			return {
 				success: true,
 				message: {
-					sender: 'bot',
-					text: responseMessage
-				}
+					role: 'assistant',
+					context: responseMessage
+				},
+				context: fullContext
 			};
 		} catch (error: unknown) {
 			setFlash(
