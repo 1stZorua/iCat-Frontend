d9aae410e4476ca9f078d95e3177b3bda2e1aac4
2ndZorua <485892@student.fontys.nl>
2024-11-19 13:43:58 +0100

    Modified the link to the prediction API

--- src/routes/scan/+page.server.ts
+++ src/routes/scan/+page.server.ts
@@ -9,7 +9,7 @@ export const actions = {
         const startTime = Date.now();
 
         try {
-            const response = await fetch('https://dss3lpmv-5000.euw.devtunnels.ms/predict', {
+            const response = await fetch('https://klp4cbg3-5000.euw.devtunnels.ms/predict', {
                 method: 'POST',
                 body: data
             });
