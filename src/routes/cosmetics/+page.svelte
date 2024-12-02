6cd500f353bb82b9c980aa6055245accc50a5dfe
Zorua <milandehaart@live.nl>
2024-12-02 20:54:48 +0100

    Added cosmetics page

---
+++ src/routes/cosmetics/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+import { PageLayout } from '$components/page';
+import { Avatar, Card, Switch } from '$components/shared/other';
+import type { CardProps } from '$components/shared/other/Card/variants';
+import { TextBase, TextSmall } from '$components/shared/text';
+import { fade } from 'svelte/transition';
+
+let activeSwitchIndex = $state(0);
+
+const categories: {
+	name: string;
+	items: {
+		color: CardProps['color'];
+		src: string;
+		href: string;
+		title: string;
+		description: string;
+	}[];
+}[] = [
+	{
+		name: 'User',
+		items: [
+			{
+				color: 'primary',
+				src: 'chat',
+				href: 'cosmetics/chat/background',
+				title: 'Background',
+				description: 'Customize your chat background'
+			}
+		]
+	},
+	{
+		name: 'iCat',
+		items: []
+	}
+];
+</script>
+
+<PageLayout page="Cosmetics">
+	<Switch
+		items={categories.map((c) => c.name)}
+		changeSwitch={(activeIndex) => activeSwitchIndex = activeIndex}
+	></Switch>
+	{#each categories as cat, index}
+		{#if index === activeSwitchIndex}
+			<div in:fade>
+				{#each cat.items as item}
+					<Card
+						href={item.href}
+						className="rounded-md bg-light-background-primary flex items-center gap-3"
+					>
+						<Avatar
+							className="flex-shrink-0 justify-center items-center bg-light-cards-{item.color}-bg rounded-sm"
+							imageClassName="h-1/2"
+							src="/images/{item.src}.png"
+						></Avatar>
+						<div class="flex flex-col text-light-text-primary">
+							<TextBase>{item.title}</TextBase>
+							<TextSmall className="text-light-text-muted">{item.description}</TextSmall>
+						</div>
+					</Card>
+				{/each}
+			</div>
+		{/if}
+	{/each}
+</PageLayout>
