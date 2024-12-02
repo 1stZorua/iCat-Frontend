a2fcc205ea0b9e82e630fdd0747dcbd029a6169e
Zorua <milandehaart@live.nl>
2024-12-02 20:48:01 +0100

    Added Switch component as a new entry.

--- src/lib/components/shared/other/index.ts
+++ src/lib/components/shared/other/index.ts
@@ -2,3 +2,4 @@ export { default as Card } from './Card/Card.svelte';
 export { default as Avatar } from './Avatar.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as Input } from './Input.svelte';
+export { default as Switch } from './Switch.svelte';
