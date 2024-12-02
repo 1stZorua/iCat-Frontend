78854d3d9843d542bd5952c73367363b1c1fab6d
Zorua <milandehaart@live.nl>
2024-12-02 20:52:19 +0100

    Removed comments and unnecessary code for quiz handling

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -4,33 +4,26 @@ import { getEmbeddingsFromJina, fetchPineconeResults, sendToOpenAI } from '$lib/
 import type { Message, VectorDBResult } from '$lib/types/types';
 
 export const actions = {
-	chat: async ({ request, fetch, cookies } : RequestEvent) => {
+	chat: async ({ request, fetch, cookies }: RequestEvent) => {
 		const data = await request.formData();
 		const prompt = data.get('prompt') as string;
 		const history = JSON.parse(data.get('history') as string) as Message[];
-		const context = JSON.parse(data.get('context') as string) as Array<string[]> ?? '';
 
 		try {
-			// prompt = `Context:\n${context.join('\n\n')}\n\nQuestion: ${prompt}`;
-			// const fullPrompt = `Context:\n${context.join('\n\n')}\n\nQuestion: ${prompt}`;
-			
 			const queryEmbedding = await getEmbeddingsFromJina(prompt);
 			const vectorDBResults = await fetchPineconeResults(fetch, queryEmbedding, 16);
 			const contextTexts = vectorDBResults.matches.map(
 				(match: VectorDBResult) => match.metadata.text
 			);
 
-			const fullContext = context.concat(contextTexts);
-
-			const responseMessage = await sendToOpenAI(prompt, contextTexts, history.slice(2, history.length));
+			const responseMessage = await sendToOpenAI(prompt, contextTexts, history);
 
 			return {
 				success: true,
 				message: {
 					role: 'assistant',
 					context: responseMessage
-				},
-				context: fullContext
+				}
 			};
 		} catch (error: unknown) {
 			setFlash(
