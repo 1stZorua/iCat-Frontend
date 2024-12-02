91da62385c585372a7d39ebb36e4fbdf1a18055d
Zorua <milandehaart@live.nl>
2024-12-02 20:48:14 +0100

    Added toProperCase function as a new entry.

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export { getEmbeddingsFromJina } from './jina';
 export { cn } from './merge';
 export { sendToOpenAI } from './openai';
 export { fetchPineconeResults } from './pinecone';
+export { toProperCase } from './text';
