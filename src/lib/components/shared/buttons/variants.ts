5e3b129f97ec619161fa45be6819dac1d9c12680
Zorua <milandehaart@live.nl>
2024-12-02 20:47:32 +0100

    Slightly updated primary and text variants for buttons

--- src/lib/components/shared/buttons/variants.ts
+++ src/lib/components/shared/buttons/variants.ts
@@ -4,11 +4,11 @@ const buttonVariants = tv({
 	base: 'flex px-6 py-2',
 	variants: {
 		variant: {
-			primary: 'bg-light-btn-primary-bg text-light-btn-primary-fg rounded-md',
+			primary: 'py-4 px-12 bg-light-btn-primary-bg text-light-btn-primary-fg rounded-md',
 			secondary: 'bg-light-btn-secondary-bg text-light-btn-secondary-fg rounded-full',
 			action:
 				'flex items-center justify-center bg-light-background-primary h-12 w-12 shadow-md rounded-full',
-			text: 'bg-light-btn-text-bg text-light-btn-text-fg'
+			text: 'p-0 bg-light-btn-text-bg text-light-btn-text-fg'
 		}
 	},
 	defaultVariants: {
