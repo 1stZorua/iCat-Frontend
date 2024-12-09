c93fafccb17a83eff39b6afac971a3bcd11b50eb
Zorua <milandehaart@live.nl>
2024-12-09 19:11:40 +0100

    Added user language and paraglide prop to Locals interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,21 @@
+import type { AvailableLanguageTag } from '../../lib/paraglide/runtime';
+import type { ParaglideLocals } from '@inlang/paraglide-sveltekit';
 // See https://svelte.dev/docs/kit/types#app.d.ts
 
-import type { FlashType } from '$lib/types/types';
+import type { FlashType, Language } from '$lib/types/types';
 
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
+			paraglide: ParaglideLocals<AvailableLanguageTag>;
 			user: {
 				age: number | null;
 				cosmetics: {
 					selectedBackground: number;
 				};
+				language: Language | undefined;
 			};
 		}
 		interface PageData {
@@ -21,6 +25,7 @@ declare global {
 				cosmetics: {
 					selectedBackground: number;
 				};
+				language: Language | undefined;
 			};
 		}
 		// interface PageState {}
