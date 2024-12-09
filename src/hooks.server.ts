444f07145b80650bf3f4170781c5539b2574139f
Zorua <milandehaart@live.nl>
2024-12-09 19:13:07 +0100

    Integrated i18n for language handling based on user selection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,32 @@
-import type { Handle } from '@sveltejs/kit';
+import { i18n } from '$lib/i18n';
+import { sequence } from '@sveltejs/kit/hooks';
+import type { Language } from '$lib/types/types';
+import { type Handle } from '@sveltejs/kit';
+import { switchToLanguage } from '$lib/utils/lang';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const customHandle: Handle = async ({ event, resolve }) => {
 	const age = event.cookies.get('age');
 	const selectedBackground = event.cookies.get('selectedBackground');
+	const langCookie = event.cookies.get('language') as string;
+	const language = langCookie
+		? (JSON.parse(event.cookies.get('language') as string) as Language)
+		: undefined;
+
+	const requestedLanguage = event.url.pathname.split('/')[1] as string;
+
+	if (language && requestedLanguage !== language.lang) {
+		switchToLanguage(event.url.pathname, language?.lang, true);
+	}
 
 	event.locals.user = {
 		age: age ? parseInt(age, 10) : null,
 		cosmetics: {
 			selectedBackground: selectedBackground ? parseInt(selectedBackground, 10) : 0
-		}
+		},
+		language: language
 	};
 
 	return await resolve(event);
 };
+
+export const handle = sequence(customHandle, i18n.handle());
