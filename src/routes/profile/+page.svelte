ca9d8563a9a8120ec0f9acd5c0af5ea466af2682
Zorua <milandehaart@live.nl>
2024-12-09 19:21:52 +0100

    Replaced hardcoded texts with dynamic translations using Paraglide and modified the general button href

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,10 +3,11 @@ import { enhance } from '$app/forms';
 import { PageLayout } from '$components/page';
 import { ButtonAction, ButtonText } from '$components/shared/buttons';
 import { Avatar, Card, Icon } from '$components/shared/other';
-import { TextBase, TextHeading, TextLarge, TextMedium, TextSmall } from '$components/shared/text';
+import { TextBase, TextHeading, TextLarge, TextSmall } from '$components/shared/text';
+import * as m from '$lib/paraglide/messages';
 </script>
 
-<PageLayout page="Profile">
+<PageLayout page={m.profile_page()}>
 	<div class="flex w-full flex-col items-center gap-5">
 		<div class="relative w-max">
 			<Avatar
@@ -20,7 +21,7 @@ import { TextBase, TextHeading, TextLarge, TextMedium, TextSmall } from '$compon
 		</div>
 		<div class="flex flex-col text-center">
 			<TextLarge className="font-semibold">Emma</TextLarge>
-			<TextBase className="text-light-text-muted font-normal">Member since January 2025</TextBase>
+			<TextBase className="text-light-text-muted font-normal">{m.profile_join_date()}</TextBase>
 		</div>
 	</div>
 	<div class="flex w-full gap-4">
@@ -29,36 +30,36 @@ import { TextBase, TextHeading, TextLarge, TextMedium, TextSmall } from '$compon
 			props={{ color: 'primary' }}
 		>
 			<TextHeading>7</TextHeading>
-			<TextSmall className="">Correct Answers</TextSmall>
+			<TextSmall className="">{m.profile_correct_answers()}</TextSmall>
 		</Card>
 		<Card
 			className="flex flex-col justify-center text-center w-full rounded-md p-6"
 			props={{ color: 'accent' }}
 		>
 			<TextHeading>105</TextHeading>
-			<TextSmall className="">Total Coins</TextSmall>
+			<TextSmall className="">{m.profile_total_coins()}</TextSmall>
 		</Card>
 	</div>
 	<div class="w-full rounded-md bg-light-background-secondary">
 		<ButtonText className="flex items-center w-full gap-4 p-4">
 			<Icon className="text-md text-light-cards-neutral-bg" icon="mingcute:shield-shape-fill"
 			></Icon>
-			<TextBase>Personal</TextBase>
+			<TextBase>{m.profile_personal()}</TextBase>
 			<Icon className="text-md ml-auto" icon="lucide:chevron-right"></Icon>
 		</ButtonText>
-		<ButtonText className="flex items-center w-full gap-4 p-4">
+		<ButtonText href="profile/general" className="flex items-center w-full gap-4 p-4">
 			<Icon className="text-md text-light-cards-primary-bg" icon="material-symbols:settings"></Icon>
-			<TextBase>General</TextBase>
+			<TextBase>{m.profile_general()}</TextBase>
 			<Icon className="text-md ml-auto" icon="lucide:chevron-right"></Icon>
 		</ButtonText>
 		<ButtonText className="flex items-center w-full gap-4 p-4">
 			<Icon className="text-md text-light-cards-secondary-bg" icon="material-symbols:help"></Icon>
-			<TextBase>Help</TextBase>
+			<TextBase>{m.profile_help()}</TextBase>
 			<Icon className="text-md ml-auto" icon="lucide:chevron-right"></Icon>
 		</ButtonText>
 		<ButtonText className="flex items-center w-full gap-4 p-4">
 			<Icon className="text-md text-light-cards-accent-bg" icon="material-symbols:call"></Icon>
-			<TextBase>Contact</TextBase>
+			<TextBase>{m.profile_contact()}</TextBase>
 			<Icon className="text-md ml-auto" icon="lucide:chevron-right"></Icon>
 		</ButtonText>
 		<form action="?/logout" method="post" use:enhance>
@@ -67,7 +68,7 @@ import { TextBase, TextHeading, TextLarge, TextMedium, TextSmall } from '$compon
 					className="text-md text-light-cards-background-bg"
 					icon="material-symbols:logout-rounded"
 				></Icon>
-				<TextBase>Logout</TextBase>
+				<TextBase>{m.profile_logout()}</TextBase>
 				<Icon className="text-md ml-auto" icon="lucide:chevron-right"></Icon>
 			</ButtonText>
 		</form>
