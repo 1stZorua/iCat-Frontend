7dda1ef9832c5d4cfd65ef19e5a5c1dd44d771ca
Zorua <milandehaart@live.nl>
2024-12-09 19:19:56 +0100

    Set default language cookie to 'nl-nl' on profile creation

--- src/routes/introduction/create/+page.server.ts
+++ src/routes/introduction/create/+page.server.ts
@@ -1,12 +1,15 @@
 import { redirect } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
+import type { Language } from '$lib/types/types';
 
 export const actions = {
 	create: async ({ request, cookies }: RequestEvent) => {
 		const data = await request.formData();
 		const age: string = data.get('age') as string;
+		const language: Language = { lang: 'nl-nl', name: 'Nederlands', icon: 'netherlands' };
 
 		cookies.set('age', age, { path: '/', httpOnly: true });
+		cookies.set('language', JSON.stringify(language), { path: '/', httpOnly: true });
 		throw redirect(303, '/');
 	}
 };
