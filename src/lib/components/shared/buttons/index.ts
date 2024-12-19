625d26dd315299cdd9226784bd7cf103d075b815
Zorua <milandehaart@live.nl>
2024-12-20 00:42:38 +0100

    Added ButtonSwitch as a new entry

--- src/lib/components/shared/buttons/index.ts
+++ src/lib/components/shared/buttons/index.ts
@@ -2,4 +2,5 @@ export { default as Button } from './Base.svelte';
 export { default as ButtonAction } from './variants/Action.svelte';
 export { default as ButtonPrimary } from './variants/Primary.svelte';
 export { default as ButtonSecondary } from './variants/Secondary.svelte';
+export { default as ButtonSwitch } from './variants/Switch.svelte';
 export { default as ButtonText } from './variants/Text.svelte';
