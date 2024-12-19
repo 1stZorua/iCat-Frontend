9f4afc8167789f51418e54023ccedcde1d17591a
Zorua <milandehaart@live.nl>
2024-12-20 00:45:35 +0100

    Renamed Switch component to TabControl

--- src/lib/components/shared/other/TabControl.svelte
+++ src/lib/components/shared/other/TabControl.svelte
@@ -3,37 +3,37 @@ import type { ClassType } from '$components/_types';
 import { cn } from '$lib/utils';
 import { TextBase } from '../text';
 
-function onSwitch(index: number) {
+function onTabChange(index: number) {
 	if (activeIndex == index) return;
 	activeIndex = index;
-	changeSwitch(index);
+	changeTab(index);
 }
 
 let {
 	className,
 	items,
-	changeSwitch
-}: { className?: ClassType; items: string[]; changeSwitch: (index: number) => void } = $props();
+	changeTab
+}: { className?: ClassType; items: string[]; changeTab: (index: number) => void } = $props();
 
 let activeIndex: number = $state(0);
-let switchContainer: HTMLDivElement | null = $state(null);
+let container: HTMLDivElement | null = $state(null);
 let width: number = $derived(100 / items.length);
 let offset = $derived.by(() => {
-	const itemWidth = (switchContainer?.offsetWidth ?? 0) / items.length;
+	const itemWidth = (container?.offsetWidth ?? 0) / items.length;
 	return itemWidth * activeIndex;
 });
 </script>
 
 <div
-	bind:this={switchContainer}
-	class={cn("relative isolate flex w-full justify-between overflow-hidden rounded-full bg-light-background-secondary py-4", className)}
+	bind:this={container}
+	class={cn("relative isolate flex w-full justify-between overflow-hidden rounded-full bg-light-background-secondary", className)}
 >
 	<div
 		class="absolute inset-0 -z-10 h-full rounded-full bg-light-btn-primary-bg transition-all"
 		style="width: {width}%; left: {offset}px"
 	></div>
 	{#each items as item, index}
-		<button onclick={() => onSwitch(index)} class="w-full">
+		<button onclick={() => onTabChange(index)} class="w-full rounded-full py-4">
 			<TextBase className={activeIndex === index ? 'text-light-text-accent' : ''}>{item}</TextBase>
 		</button>
 	{/each}
