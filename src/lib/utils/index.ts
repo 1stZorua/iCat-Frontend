4e0cdfa543897348962083d092a1040490fd4abc
Zorua <milandehaart@live.nl>
2024-12-20 00:49:16 +0100

    Added copyToClipboard, initTTS, stopTTS, TTS, getPermissionState, getCameraPermissionState, and formatDate as new entries

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,9 @@
+export { formatDate } from './date';
 export { getEmbeddingsFromJina } from './jina';
 export { captureImageFromCanvas, createFileFromBlob } from './media';
 export { cn } from './merge';
 export { sendToOpenAI } from './openai';
+export { getPermissionState, getCameraPermissionState } from './permission';
 export { fetchPineconeResults } from './pinecone';
-export { toProperCase } from './text';
+export { toProperCase, copyToClipboard } from './text';
+export { initTTS, stopTTS, TTS } from './tts';
