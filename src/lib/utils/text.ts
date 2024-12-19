c548b080f4f395fa5e613d87935d9267a936fe33
Zorua <milandehaart@live.nl>
2024-12-20 00:52:10 +0100

    Added copyToClipboard function for clipboard text copying

--- src/lib/utils/text.ts
+++ src/lib/utils/text.ts
@@ -1,3 +1,11 @@
 export function toProperCase(input: string[]): string {
 	return input.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 }
+
+export async function copyToClipboard(text: string): Promise<void> {
+	try {
+		await navigator.clipboard.writeText(text);
+	} catch (error) {
+		console.error(error);
+	}
+}
