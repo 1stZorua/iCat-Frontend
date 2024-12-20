993ba5b2ccf0baa6a7484e20694ee683b9b78e7e
Zorua <milandehaart@live.nl>
2024-12-20 01:01:19 +0100

    Refactored code for readability and added temporary placeholder for iCat cosmetics

--- src/routes/cosmetics/+page.svelte
+++ src/routes/cosmetics/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 import { PageLayout } from '$components/page';
-import { Avatar, Card, Switch } from '$components/shared/other';
+import { Avatar, Card, TabControl } from '$components/shared/other';
 import type { CardProps } from '$components/shared/other/Card/variants';
 import { TextBase, TextSmall } from '$components/shared/text';
 import { fade } from 'svelte/transition';
 import * as m from '$lib/paraglide/messages';
 
-let activeSwitchIndex = $state(0);
+let activeTabIndex = $state(0);
 
 const categories: {
 	name: string;
@@ -24,40 +24,48 @@ const categories: {
 			{
 				color: 'primary',
 				src: 'chat',
-				href: `cosmetics/chat/${m.cosmetics_href()}`,
-				title: m.cosmetics_heading(),
-				description: m.cosmetics_description()
+				href: `cosmetics/chat/${m.cosmetics_background_href()}`,
+				title: m.cosmetics_background_heading(),
+				description: m.cosmetics_background_description()
 			}
 		]
 	},
 	{
 		name: m.cosmetics_icat(),
-		items: []
+		items: [
+			{
+				color: 'background',
+				src: 'error',
+				href: m.cosmetics_icat_href(),
+				title: m.cosmetics_icat_heading(),
+				description: m.cosmetics_icat_description()
+			}
+		]
 	}
 ];
 </script>
 
 <PageLayout page={m.cosmetics_name()}>
-	<Switch
+	<TabControl
 		items={categories.map((c) => c.name)}
-		changeSwitch={(activeIndex) => activeSwitchIndex = activeIndex}
-	></Switch>
+		changeTab={(activeIndex) => activeTabIndex = activeIndex}
+	></TabControl>
 	{#each categories as cat, index}
-		{#if index === activeSwitchIndex}
+		{#if index === activeTabIndex}
 			<div in:fade>
-				{#each cat.items as item}
+				{#each cat.items as { color, src, href, title, description }}
 					<Card
-						href={item.href}
+						href={href}
 						className="rounded-md bg-light-background-primary flex items-center gap-3"
 					>
 						<Avatar
-							className="flex-shrink-0 justify-center items-center bg-light-cards-{item.color}-bg rounded-sm"
+							className="flex-shrink-0 justify-center items-center bg-light-cards-{color}-bg rounded-sm"
 							imageClassName="h-1/2"
-							src="/images/{item.src}.png"
+							src="/images/{src}.png"
 						></Avatar>
 						<div class="flex flex-col text-light-text-primary">
-							<TextBase>{item.title}</TextBase>
-							<TextSmall className="text-light-text-muted">{item.description}</TextSmall>
+							<TextBase>{title}</TextBase>
+							<TextSmall className="text-light-text-muted">{description}</TextSmall>
 						</div>
 					</Card>
 				{/each}
