67b2c17a6fb0284d0789cbd0da009371ec2f074b
Zorua <milandehaart@live.nl>
2024-12-20 01:04:52 +0100

    Removed camera and joinedDate cookies on logout

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,6 +4,8 @@ export const actions = {
 	logout: async ({ cookies }) => {
 		cookies.delete('age', { path: '/' });
 		cookies.delete('selectedBackground', { path: '/' });
+		cookies.delete('camera', { path: '/' });
+		cookies.delete('joinedDate', { path: '/' });
 
 		throw redirect(303, '/');
 	}
